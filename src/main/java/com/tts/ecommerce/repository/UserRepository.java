99178d80c973d58a8458b3203a1c7fb3a993d088
Joey Whalen <jtktwhalen@gmail.com>
2021-03-09 10:34:14 -0500

    Updated UserRepository

--- src/main/java/com/tts/ecommerce/repository/UserRepository.java
+++ src/main/java/com/tts/ecommerce/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.tts.ecommerce.repository;
 
-public interface UserRepository {
+import com.tts.ecommerce.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+  User findByUsername(String username);
 }
