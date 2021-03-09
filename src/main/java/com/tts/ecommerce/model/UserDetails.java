81d83b6f2bc0f8bc8b2279f8f275b96cd9b6748a
Joey Whalen <jtktwhalen@gmail.com>
2021-03-09 10:33:49 -0500

    Created UserDetails.java

--- src/main/java/com/tts/ecommerce/model/UserDetails.java
+++ src/main/java/com/tts/ecommerce/model/UserDetails.java
@@ -1,2 +1,19 @@
-package com.tts.ecommerce.model;public class UserDetails {
+package com.tts.ecommerce.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Transient;
+import java.util.Collection;
+
+public class UserDetails {
+  @Transient
+  private boolean accountNonExpired = true;
+  @Transient
+  private boolean accountNonLocked = true;
+  @Transient
+  private boolean credentialNonExpired = true;
+  @Transient
+  private boolean enabled = true;
+  @Transient
+  private Collection <GrantedAuthority> authorities = null;
 }
